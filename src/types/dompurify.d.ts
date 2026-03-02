4bee88bacd4c1230f79e524c651c8aa7e7b29375
Mark <mark@remarkablemark.org>
2026-03-01 19:10:35 -0500

    chore(types): remove unnecessary dompurify.d.ts

--- src/types/dompurify.d.ts
+++
@@ -1,7 +0,0 @@
-import DOMPurify from 'dompurify';
-
-export type SanitizedHTML = ReturnType<typeof DOMPurify.sanitize>;
-
-declare module 'dompurify' {
-  export function sanitize(html: string): SanitizedHTML;
-}
